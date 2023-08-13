81d510bb446f854d0c6f6374ead310a2c6edb381
Raj Aryan <rajxryn@gmail.com>
2023-08-13 18:32:36 +0530

    Add Basic Meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,39 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Udaan AeroModelling Club | NITR ',
-  description: ' We are a passionate group of engineers and aviation enthusiasts dedicated to the world of drones, RC planes, and rockets. Our club is a hub for the sharing of innovative ideas and the exchange of effective knowledge, favoring practical and theoretical advances in remodeling designs.',
-}
+  title: "Udaan AeroModelling Club | NITR ",
+  description:
+    " The Official AeroModelling Club Of NITR | Group of engineers and aviation enthusiats",
+  keywords: ["Udaan ", "RC Drones", "Rockets", "Planes", "RC Planes"],
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://udaan/",
+    title: "Udaan AeroModelling Club | NITR",
+    description:
+      " The Official AeroModelling Club Of NITR | Group of engineers and aviation enthusiats",
+    image: "https://udaan/og.png",
+    site_name: "Udaan AeroModelling Club | NITR",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Udaan AeroModelling Club | NITR",
+    description:
+      "The Official AeroModelling Club Of NITR | Group of engineers and aviation enthusiats",
+    images: {
+      url: "https://udaan/og.png",
+      alt: "Udaan AeroModelling Club | NITR",
+    },
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
